9ceb76e048c54f430465bbf39c2ffcf00f409992
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-02-04 14:27:08 -0500

    license year update

--- srt.lua
+++ srt.lua
@@ -5,7 +5,7 @@ local srt = {
 	_LICENSE		= [[
 	MIT License
 
-	Copyright (c) 2023 alejandro-alzate
+	Copyright (c) 2024 alejandro-alzate
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
